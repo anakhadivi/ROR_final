d39ccc5da1e2ca49c6e92e014ddcdc6356f21fb4
Ana Khadivi <anakhadivi@icloud.com>
2014-04-17 11:31:48 -0400

    initial commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,27 @@
+class PostsController < ApplicationController
+	def index
+		@posts = Post.all
+	end
+	def show
+		@post = Post.find(params[:id])	
+	end
+	def new
+		@post = Post.new
+	end
+	def create
+		@post = Post.new(post_params)
+		@post.user = current_user
+		if @post.save
+			flash[:notice] = 'Your post has been saved'
+			redirect_to posts_path
+		else
+			flash[:alert] = 'There was a problem saving your post'
+			redirect_to :back
+		end
+
+	end
+def post_params
+	params.require(:post).permit(:title, :location, :description)
+	
+end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blader::Application.config.secret_key_base = 'a8ddeccf2c2aa9d688a82ebcf996c486712771852f8b50032646bf6b9991f1ee666a9b639301fd8bc1fde40dd9f92aed21ec41dcb63bb058e5b3739389d1ba6d'
