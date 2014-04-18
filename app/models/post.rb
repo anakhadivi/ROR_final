3f3330f68b105740b75dbfd47acd43c3772c8741
Ana Khadivi <anakhadivi@icloud.com>
2014-04-17 20:23:45 -0400

    asset pipeline

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ActiveRecord::Base
 	belongs_to :user
-	has_attached_file :image
+	has_attached_file :image, :styles => { :medium => "200x200>", :thumb => "60x60>" }, 
+	:default_url => "/images/default_:style_avatar.png"
 end
